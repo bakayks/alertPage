7db1c60cd64a6dd77da5a190dc74422ec184ad25
Abdullo <abdullokh2020@gmail.com>
2020-11-19 15:00:07 +0600

    fix bug

--- index.php
+++ index.php
@@ -1,21 +1,7 @@
 <?php
     include('functions.php');
 
-    function getJSON() {
-        $url = "https://query1.finance.yahoo.com/v10/finance/quoteSummary/";
-        $param = "?modules=price";
 
-        $urlPart = $_GET['ticker-name'];
-
-        $json = file_get_contents($url . $urlPart . $param);
-        $obj = json_decode($json, true);
-
-        return $obj->result[0];
-    }
-
-    function someFunc() {
-        $object = getJSON();
-    }
 ?>
 
 <!DOCTYPE html>
@@ -38,21 +24,38 @@
 
 
     <div class="input">
-        <form id="ticker-form" method="get">
+        <form id="ticker-form" method="get" enctype="multipart/form-data">
             <input type="text" name="ticker-name" placeholder="Введите тиккер">
-            <input type="submit" onclick="someFunc();">
+            <button name="submit" type="submit" class="btn btn-primary">Добавить</button>
             <br>
             <pre>
-            <?php 
-                echo object;
-            ?>
             </pre>
         </form>
     </div> <!-- input -->
+    <?php
 
+        if (isset($_GET["submit"])){
+
+            $url = "https://query1.finance.yahoo.com/v10/finance/quoteSummary/";
+            $param = "?modules=price";
+
+            $urlPart = $_GET['ticker-name'];
+
+            echo $json = file_get_contents($url . $urlPart . $param);
+            $obj = json_decode($json, true);
+
+            return $obj->result[0];
+
+        }
+
+
+
+
+
+    ?>
     <div class="content">
         <div class="row">
-            <?php $obj ?>
+
         </div> <!-- row -->
     </div> <!-- content -->
 </body>
