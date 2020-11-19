ad9205d21314b0a44867ad87c5db712f68377f88
Bekbulat <b.tashbolotoff@mail.ru>
2020-11-19 16:42:50 +0600

    added logo and refresh

--- index.php
+++ index.php
@@ -10,27 +10,29 @@ include('functions.php');
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Alert Page</title>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
-          integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.2/css/all.min.css">
-    <link rel="stylesheet" href="style.css">
 
+    <link rel="stylesheet" href="style.css">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300&display=swap" rel="stylesheet">
 
 </head>
 <body>
-<div class="nav">
-    <a href="" class="logo">
-        <img src="" alt="">
-    </a>
-</div> <!-- nav -->
-
 <div class="container">
+        <div class="nav">
+            <a href="#" class="logo">
+                <img src="images/kvaal.svg" alt="" style="height: 70px; width: 150px">
+            </a>
+            <a href="#" id="refresh">Обновить котировки</a>
+        </div>
+
+
     <div class="input">
-        <form id="ticker-form" class="form-inline" method="get" enctype="multipart/form-data">
-            <div class="form-group mx-sm-3 mb-2">
-                <input type="text" class="form-control" name="ticker-name" placeholder="Введите тиккер">
-            </div>
-            <a name="submit" type="submit" ><img src="https://static.tildacdn.com/tild6437-3861-4736-a663-363438623137/krest.svg" alt=""></a>
+        <form id="ticker-form" method="get" enctype="multipart/form-data">
+            <input type="text" name="ticker-name" placeholder="Введите тиккер">
+            <button name="submit" type="submit" class="btn btn-primary">Добавить</button>
+            <br>
+            <pre>
+            </pre>
         </form>
     </div>
     <?php
@@ -62,23 +64,14 @@ include('functions.php');
 
     }
     ?>
+    <div class="content">
+        <div class="row">
 
+        </div> <!-- row -->
+    </div> <!-- content -->
 </div>
-<div class="content">
-    <div class="row">
-
-    </div> <!-- row -->
-</div> <!-- content -->
 </body>
-<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
-        integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN"
-        crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"
-        integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q"
-        crossorigin="anonymous"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"
-        integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl"
-        crossorigin="anonymous"></script>
+
 <script src="http://code.jquery.com/jquery-latest.js"></script>
 <script src="script.js"></script>
 </html>
